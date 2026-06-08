dc43b05ed274e730a55b4b6f494752a162aa8cf3
picafern-commits <pica.fern@gmail.com>
2026-06-08 19:32:12 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.7.3';
+const APP_VERSION = '1.7.4';
 const STORAGE_KEY = 'autoparts_callcenter_v1';
 const SESSION_KEY = 'autoparts_callcenter_session_user';
 const FIREBASE_LEGACY_STATE_COLLECTION = 'appState';
